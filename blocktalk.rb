74751870a6345bb2cbd98fa2b8df3e5372a00b3d
Eric Levine <djlevine@gmail.com>
2013-06-26 15:38:32 -0500

    Changed functionality to 'pick 3'

--- blocktalk.rb
+++ blocktalk.rb
@@ -6,11 +6,17 @@
 # Has certain available times, others unavailable.
 # Times can be in an array
 
-# API: Gmail will send an email confirmation of the meeting.
+# Scheduler's job is to compare availabilities between 2 users and return mutual 
+# availability times. 
+
+# Notifier
+# Confirms appts with Gmail API: Gmail will send an email confirmation of the meeting.
+
+requre "gmail"
 
 class User
 
-# HANDLES USER AVAILABILITY AND APPOINTMENTS
+	# HANDLES USER INFORMATION
 
 	@@users = []
 
@@ -22,8 +28,9 @@ class User
 
 		@times_avail = [
 			# Sunday - [0]
-			["900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", 
-			"1700"],
+			["900" => false, "1000" => false, "1100" => false, "1200" => false, 
+				"1300" => false, "1400" => false, "1500" => false, "1600" => false, 
+			"1700" => false],
 
 			# # #Monday - [1]
 			["900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", 
@@ -50,7 +57,7 @@ class User
 			"1700"],
 		]
 		
-		@appointments = {}
+		@appointments = []
 
 		@@users << self
 	end
@@ -69,15 +76,19 @@ end
 
 class Blocktalk
 
+	def self.day_indeces (day)
+		indeces = {"Sunday" => 0, "Monday" => 1, "Tuesday" => 2, "Wednesday" => 3, 
+			"Thursday" => 4, "Friday" => 5, "Saturday" => 6}
+		day_index = indeces[day]
+	end
+
 	# BOOKS USERS, SENDS CONFIRMATION
 
-	# require _________
-
-	def is_available? (user1, avail_day_index, avail_time)
+	def self.is_available? (user1, avail_day_index, avail_time)
 		user1.times_avail[avail_day_index].include?(avail_time)
 	end
 
-	def mutual_avail? (user1, user2, avail_day_index, time)
+	def self.mutual_avail? (user1, user2, avail_day_index, time)
 		if user1.is_available?(avail_day_index, time) == true && 
 			user2.is_available?(avail_day_index, time) == true
 			return true
@@ -86,24 +97,71 @@ class Blocktalk
 		end
 	end
 
-	def book_appt 
-		if mutual_avail? () == true
-	# 	end
+	# def book_appt 
+	# 	if mutual_avail? () == true
+	# # end
 	# end
 
 
 	def self.execute
+		users = []
 		puts "Welcome to BlockTalk, what is your name?"
 		username = gets.chomp
-		User.new(username)
-		puts "Hello #{username}. What is your email?"
-		@email = gets.chomp!
+		user1 = User.new(username)
+		
 
-		# puts "Great! We've marked you as available Sun-Sat from 9a-5p. If you wish
-		# to edit your schedule, please type '______'"
+		puts "Hello #{user1.username}. What is your email?"
+		user1.email = gets.chomp!
 
-		# puts "Who would you like to speak with?"
-		# user2 = gets.chomp!
+		users << user1
+		
+		puts "Who would you like to speak with?"
+		user2 = gets.chomp!
+		puts "What is their email address?"
+		user2_email = gets.chomp!
+		puts "Let's create 3 time blocks that work for you. What day would you like
+		to meet #{user2}?"
+		day1 = gets.chomp!
+		puts "OK, what time on #{day1}?"
+		time1 = gets.chomp!
+		puts "Great. In case #{user2} can't do then, please add an alternative time. 
+		Let's start with the day."
+		day2 = gets.chomp!
+		puts "and a time on #{day2}?"
+		time2 = gets.chomp!
+		puts "Last time. Let's choose an alternative time to meet. Day?"
+		day3 = gets.chomp!
+		puts "...and time on #{day3}?"
+		time3 = gets.chomp!
+
+		@timeblocks = []
+		@timeblocks << {day1 => time1, day2 => time2, day3 => time3}
+
+		puts "Excellent! We have sent an email to #{user2} at #{user2_email}. "
+
+
+		# puts "Great! We've marked you as available Sun-Sat from 9a-5p. Do you wish
+		# to mark yourself unavailable?"
+
+		# e = gets.chomp!
+
+		# if e == "yes"
+		# 	while e == "yes"
+		# 	puts "What day are you unavailable?"
+		# 	day = gets.chomp!
+		# 	puts "At what time on #{day} are you unavailable?"
+		# 	time = gets.chomp!
+		# 		day_index = Blocktalk.day_indeces(day)
+		# 	u.make_unavailable(day_index, time)
+		# 	puts "Great. #{day} at #{time} has been removed from your availability."
+		# 	puts "#{day}" 
+		# 	puts "-------------"
+		# 	puts u.times_avail[day_index]
+		# 	puts "Are there any more times you are unavailable?"
+		# 	end
+		# end
+
+
 
 		# puts "OK, you and #{user2} are both available at the following times:"
 		# #each loop on mass mutual avail? method
@@ -128,5 +186,4 @@ class Blocktalk
 	# 	sent to #{@email}"
 
 	#Create another availability class, link them up, work with it there.
-
 end
